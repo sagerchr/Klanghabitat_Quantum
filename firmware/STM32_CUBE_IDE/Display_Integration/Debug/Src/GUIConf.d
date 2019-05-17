b4f33fec96fc43bce30e4dd722ddb66324f61b28
Christian Sager <christian.sager@etteplan.com>
2019-05-17 09:48:04 +0200

    update

---
+++ firmware/STM32_CUBE_IDE/Display_Integration/Debug/Src/GUIConf.d
@@ -0,0 +1,20 @@
+Src/GUIConf.o: ../Src/GUIConf.c ../Middlewares/ST/STemWin/inc/GUI.h \
+ ../Middlewares/ST/STemWin/inc/GUI_ConfDefaults.h ../Inc/GUIConf.h \
+ ../Middlewares/ST/STemWin/inc/GUI_Type.h \
+ ../Middlewares/ST/STemWin/inc/LCD.h \
+ ../Middlewares/ST/STemWin/inc/Global.h \
+ ../Middlewares/ST/STemWin/inc/GUI_Version.h
+
+../Middlewares/ST/STemWin/inc/GUI.h:
+
+../Middlewares/ST/STemWin/inc/GUI_ConfDefaults.h:
+
+../Inc/GUIConf.h:
+
+../Middlewares/ST/STemWin/inc/GUI_Type.h:
+
+../Middlewares/ST/STemWin/inc/LCD.h:
+
+../Middlewares/ST/STemWin/inc/Global.h:
+
+../Middlewares/ST/STemWin/inc/GUI_Version.h:
